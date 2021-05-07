aa1979193e7e7a272173e69de301e49eaa173329
Evgeny Golyshev <eugulixes@gmail.com>
2021-05-07 16:10:25 +0300

    Add stub for start remote procedure

--- server.py
+++ server.py
@@ -19,13 +19,18 @@ import signal
 
 import tornado.web
 import tornado.websocket
+from tornado import gen
 from tornado.options import options
-
+from tornado.websocket import WebSocketClosedError
 from shirow.ioloop import IOLoop
 from shirow.server import RPCServer, TOKEN_PATTERN, remote
 
+from tutorin_tech.rpc.engine import Docker
+
 
 BUF_SIZE = 65536
+
+READY_CODE = 0
 
 
 class Application(tornado.web.Application):
@@ -44,6 +49,8 @@ class StepIsInvalid(Exception):
 class TITRPCServer(RPCServer):
     def __init__(self, application, request, **kwargs):
         super().__init__(application, request, **kwargs)
+
+        self._engine = Docker()
 
         self._student_fd = self._tutor_fd = None
         self._student_pid = self._tutor_pid = None
@@ -69,10 +76,16 @@ class TITRPCServer(RPCServer):
         ]
         self._len = len(self._steps) - 1  # do not count the introduction
 
-    def _fork_pty(self):
+    def _fork_pty(self, username):
         pid, fd = pty.fork()
         if pid == 0:  # child
-            command_line = ['echo', 'Hello, World!', ]
+            command_line = [
+                'ssh',
+                '-o', 'LogLevel=quiet',
+                '-o', 'StrictHostKeyChecking=no',
+                '-p', '2222',
+                f'{username}@127.0.0.1',
+            ]
             os.execvp(command_line[0], command_line)
         else:  # parent
             return pid, fd
@@ -85,8 +98,8 @@ class TITRPCServer(RPCServer):
 
         return {**response, **kwargs}
 
-    def destroy(self):
-        """TODO: implement. """
+    async def destroy(self):
+        await self._engine.stop()
 
     @remote
     async def list_steps(self, request):
@@ -118,8 +131,15 @@ class TITRPCServer(RPCServer):
         return self._form_response(step=step)
 
     @remote
+    async def start(self, request):
+        await self._engine.start()
+        await gen.sleep(5)
+        self._pointer = 1
+        return READY_CODE
+
+    @remote
     async def read_student_fd(self, request):
-        self._student_pid, self._student_fd = self._fork_pty()
+        self._student_pid, self._student_fd = self._fork_pty('student')
 
         def student_fd_handler(*_args, **_kwargs):
             try:
@@ -127,13 +147,16 @@ class TITRPCServer(RPCServer):
             except OSError:
                 return
 
-            request.ret_and_continue(data.decode('utf8'))
+            try:
+                request.ret_and_continue(data.decode('utf8'))
+            except WebSocketClosedError:
+                return
 
         self.io_loop.add_handler(self._student_fd, student_fd_handler, self.io_loop.READ)
 
     @remote
     async def read_tutor_fd(self, request):
-        self._tutor_pid, self._tutor_fd = self._fork_pty()
+        self._tutor_pid, self._tutor_fd = self._fork_pty('tutor')
 
         def tutor_fd_handler(*_args, **_kwargs):
             try:
@@ -141,7 +164,10 @@ class TITRPCServer(RPCServer):
             except OSError:
                 return
 
-            request.ret_and_continue(data.decode('utf8'))
+            try:
+                request.ret_and_continue(data.decode('utf8'))
+            except WebSocketClosedError:
+                return
 
         self.io_loop.add_handler(self._tutor_fd, tutor_fd_handler, self.io_loop.READ)
 
